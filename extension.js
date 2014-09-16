c258b3a9bdb6c1af5d4d3f48c6caddf3d65a657f
CodingCodeChen <peter3598768@gmail.com>
2014-09-16 18:05:52 +0800

    add hover event

--- extension.js
+++ extension.js
@@ -1,5 +1,39 @@
 $(document).ready(function() {
     var enlargeIconUrl = chrome.extension.getURL("image/enlarge.png");
-    $(".image-item").find("img").attr("src", enlargeIconUrl);
-    //$(".image-item").addClass("canDownload");
+    var imageItem = $(".image-item");
+    imageItem.find("img").bind({
+        mouseenter : function() {
+            var $this = $(this);
+            var position = $this.position();
+            var enlargeElement = $("<img>", {
+                src : enlargeIconUrl,
+                class : "overlay icon",
+                id : "enlarge",
+                });
+            $this.after(enlargeElement);
+
+            //width() : element
+            //outerWidth() : (border + padding)*2 + element
+            //outerWidth(true) : (margin + border + padding)*2 + element
+            var left = position.left + ( $this.outerWidth(true) - $this.outerWidth() )/2 + $this.outerWidth() - enlargeElement.outerWidth(true);
+            enlargeElement.css({ 
+                "top" : position.top, 
+                "left" : left,
+            });
+
+            enlargeElement.bind({
+                mouseleave : function() {
+                    $(this).remove();
+                }
+            });
+        },
+        mouseleave : function() {
+            console.log("mouseleave"); 
+            var $this = $(this);
+            //if mouse on #enlarge, don't remove #enlarge
+            if(!$("#enlarge").is(":hover")) {
+                $("#enlarge").remove();
+            }
+        }
+    });
 });
