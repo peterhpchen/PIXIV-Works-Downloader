e7b472f2bab70ab8bccd970c923d09f42d3f5373
CodingCodeChen <peter3598768@gmail.com>
2014-09-17 18:07:07 +0800

    inching icon and get big picture

--- extension.js
+++ extension.js
@@ -15,20 +15,41 @@ $(document).ready(function() {
             //width() : element
             //outerWidth() : (border + padding)*2 + element
             //outerWidth(true) : (margin + border + padding)*2 + element
-            var left = position.left + ( $this.outerWidth(true) - $this.outerWidth() )/2 + $this.outerWidth() - enlargeElement.outerWidth(true);
+            var top = position.top + parseInt($this.css("border-width"));
+            var left = position.left + ( $this.outerWidth(true) - $this.outerWidth() )/2 + $this.outerWidth() - parseInt($this.css("border-width")) - enlargeElement.outerWidth(true);
             enlargeElement.css({ 
-                "top" : position.top, 
+                "top" : top, 
                 "left" : left,
             });
 
             enlargeElement.bind({
+                mouseenter : function() {
+                    var $this = $(this);
+                    $this.css("opacity", 1.0);
+                },
                 mouseleave : function() {
                     $(this).remove();
+                },
+                click : function(event) {
+                    var $this = $(this);
+                    //var href = $this.parents('.work').attr('href');
+                    var href = "http://i2.pixiv.net/img-zip-ugoira/img/2014/09/15/20/51/11/46000573_ugoira1920x1080.zip";
+                    $.get(href, function(data) {
+                        //var $parsed = $('<div>').append(data);
+                        //var src = $parsed.find('.works_display').children('a').children('img').attr('src');
+                        //var bigSrc = src.replace('_m', '');
+                        //$('body').append(
+
+                        //    $("<img>", { src : bigSrc })
+                        //);
+                        alert(data);
+                    });
+                    //alert(src);
+                    event.preventDefault();
                 }
             });
         },
         mouseleave : function() {
-            console.log("mouseleave"); 
             var $this = $(this);
             //if mouse on #enlarge, don't remove #enlarge
             if(!$("#enlarge").is(":hover")) {
