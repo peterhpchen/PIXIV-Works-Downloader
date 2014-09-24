c29697f21ca19b6e3517acb8354abdf40dce8217
CodingCodeChen <peter3598768@gmail.com>
2014-09-24 17:41:13 +0800

    integrate function and centered picture and add box

--- extension.js
+++ extension.js
@@ -1,106 +1,236 @@
 $(document).ready(function() {
 
-    //get image url
-    var enlargeIconUrl = chrome.extension.getURL("image/enlarge.png");
-    var crossIconUrl = chrome.extension.getURL("image/cross.png");
-    var loadingIconUrl = chrome.extension.getURL("image/loading.gif");
-    var downloadIconUrl = chrome.extension.getURL("image/download.png");
-    var multiDownloadIconUrl = chrome.extension.getURL("image/multiDownload.png");
-    var upIconUrl = chrome.extension.getURL("image/arrows/up.png");
-    var downIconUrl = chrome.extension.getURL("image/arrows/down.png");
-    var leftIconUrl = chrome.extension.getURL("image/arrows/left.png");
-    var rightIconUrl = chrome.extension.getURL("image/arrows/right.png");
+    //function sets
 
-    var imageItem = $(".image-item");
-    imageItem.find("img").bind({
-        mouseenter : function(event) {
+    setIconUrl = function() {
+
+        var enlargeIconUrl = chrome.extension.getURL("image/enlarge.png");
+        var crossIconUrl = chrome.extension.getURL("image/cross.png");
+        var loadingIconUrl = chrome.extension.getURL("image/loading.gif");
+        var downloadIconUrl = chrome.extension.getURL("image/download.png");
+        var multiDownloadIconUrl = chrome.extension.getURL("image/multiDownload.png");
+        var upIconUrl = chrome.extension.getURL("image/arrows/up.png");
+        var downIconUrl = chrome.extension.getURL("image/arrows/down.png");
+        var leftIconUrl = chrome.extension.getURL("image/arrows/left.png");
+        var rightIconUrl = chrome.extension.getURL("image/arrows/right.png");
+
+        return {
+            enlarge : enlargeIconUrl,
+            cross : crossIconUrl,
+            loading : loadingIconUrl,
+            download : downloadIconUrl,
+            multiDownload : multiDownloadIconUrl,
+            up : upIconUrl,
+            down : downIconUrl,
+            left : leftIconUrl,
+            right : rightIconUrl
+        };
+    }; //end setIconUrl
+
+    setBox = function(IconUrl) {
+        $('body').append(
+            $('<div>', {
+                id : "box"
+            }).append(
+                $('<img>', {
+                    id : "boxClose",
+                    class : "icon",
+                    src : IconUrl.cross
+                })
+            ).append(
+                $('<img>', {
+                    id : "boxDownload",
+                    class : "icon",
+                    src : IconUrl.download
+                })
+            ).append(
+                $('<img>', {
+                    id : "boxMultiDownload",
+                    class : "icon",
+                    src : IconUrl.multiDownload
+                })
+            ).append(
+                $('<img>', {
+                    id : "boxLoading",
+                    class : "icon",
+                    src : IconUrl.loading
+                })
+            ).append(
+                $('<div>', {
+                    id : "boxTitle"
+                })
+            ).append(
+                $('<div>', {
+                    id : "boxLeft" 
+                }).append(
+                    $('<img>', {
+                        class : "icon",
+                        src : IconUrl.left 
+                    })
+                ) //end div#boxleft append
+            ).append(
+                $('<div>', {
+                    id : "boxRight"
+                }).append(
+                    $('<img>', {
+                        class : "icon",
+                        src : IconUrl.right
+                    })
+                ) //end div#boxleft append
+            ).append(
+                $('<div>', {
+                    id : "boxUp" 
+                }).append(
+                    $('<img>', {
+                        class : "icon",
+                        src : IconUrl.up
+                    })
+                ) //end div#boxleft append
+            ).append(
+                $('<div>', {
+                    id : "boxDown"
+                }).append(
+                    $('<img>', {
+                        class : "icon",
+                        src : IconUrl.down
+                    })
+                ) //end div#boxleft append
+            ).append(
+                $('<div>', {
+                    id : "boxContent"
+                })
+            ) //end div#box append
+        ); //end body append box
+
+        $('body').append(
+            $('<div>')
+                .attr('id', 'boxShadow')
+                .css({
+                    'height' : window.innerHeight,
+                    'width' : window.innerWidth,
+                })
+        ); //end body append boxShadow
+
+    }; //end setBox
+
+    initialEnlarge = function($enlarge) {
+        $enlarge.css({
+            "display" : "none",
+            "opacity" : 0.4
+        }).unbind();
+    }; //end initialEnlarge
+
+    setThumbnail = function() {
+         
+        var $thumbnails = $("._thumbnail"); //get img._thumbnail (small picture)
+
+        $thumbnails.hover(
+            function(event) {
+                var $this = $(this); //thumbnail
+                var $enlarge = $('#enlarge'); //enlargeIcon
+                var offsetOfThumbnail = $this.offset(); //absolute position
+
+                //width() : element
+                //outerWidth() : (border + padding)*2 + element
+                //outerWidth(true) : (margin + border + padding)*2 + element
+                var enlargeTop = offsetOfThumbnail.top;
+                var enlargeLeft = offsetOfThumbnail.left + $this.outerWidth() - $enlarge.outerWidth(true);
+                $('#enlarge').css({
+                    "display" : "inline",
+                    "top" : enlargeTop,
+                    "left" : enlargeLeft
+                });
+
+                setEnlargeEvent($enlarge, $this);
+            },
+            function() {
+                $enlarge = $('#enlarge');
+                //if mouse on #enlarge, don't remove #enlarge
+                if(!$enlarge.is(":hover")) {
+                    initialEnlarge($enlarge);
+                }
+            }); //end hover
+    }; //end setThumbnail
+
+    setEnlargeEvent = function($enlarge, $thumbnail) {
+
+        $enlarge.hover(
+            function() {
+                $(this).css("opacity", 1.0);
+            },
+            function() {
+                initialEnlarge($(this));
+            }
+        ).click($thumbnail, function(event) {
             var $this = $(this);
-            var position = $this.offset();
-            var enlargeElement = $("<img>", {
-                src : enlargeIconUrl,
+            var $target = $(event.data);
+            var href = $target.closest('a').attr('href');
+
+            //get thumbnail coordinate, width and height
+            var thumbnailTop = $target.top;
+            var thumbnailLeft = $target.left;
+            var thumbnailWidth = $target.width();
+            var thumbnailHeight = $target.height();
+            //end get thumbnail coordinate, width and height
+
+            $target.resize(function() {
+                $this = $(this);
+                $this.css({
+                    'top' : Math.floor((window.innerHeight - $this.height())/2),
+                    'left' : Math.floor(( window.innerWidth - $this.width())/2)
+                });
+            });
+            $.get(href, function(data) {
+                var $parsed = $('<div>').append(data);
+                var medienSrc = $parsed.find('.works_display').children('a').children('img').attr('src');
+                var bigSrc = medienSrc.replace('_m', '');
+                $target.attr('src', medienSrc).load(function() {
+                    $target.resize();
+                });
+            });
+            $target.css({
+                'position' : 'fixed',
+                'z-index' : 10000,
+                //'width' : window.innerWidth,
+                //'height' : window.innerHeight,
+                'top' : Math.floor((window.innerHeight - thumbnailHeight)/2),
+                'left' : Math.floor(( window.innerWidth - thumbnailWidth)/2)
+            });
+            $('#boxShadow').css("display", "inline")
+            .click({'target' : $target, 'thumbnailTop' : thumbnailTop, 'thumbnailLeft' : thumbnailLeft}, function(event) {
+                var $target = event.data.target;
+                var thumbnailTop = event.data.thumbnailTop;
+                var thumbnailLeft = event.data.thumbnailLeft;
+                $target.css({
+                    'position' : 'static',
+                    'width' : thumbnailWidth,
+                    'height' : thumbnailHeight,
+                    'top' : thumbnailTop,
+                    'left' : thumbnailLeft
+                });
+                $(this).css("display", "none");
+            });
+            initialEnlarge($this);
+            event.preventDefault();
+        });
+    }; //end setEnlargeEvent
+
+    setEnlarge = function(IconUrl) {
+        $('body').append(
+            $("<img>", {
+                src : IconUrl.enlarge,
                 class : "overlay icon",
                 id : "enlarge",
                 title : "Enlarge",
-                //'data-url' : $this.parents('.work').attr('href')
-                });
-            $('body').append(enlargeElement);
-            //width() : element
-            //outerWidth() : (border + padding)*2 + element
-            //outerWidth(true) : (margin + border + padding)*2 + element
-            var top = position.top;
-            var left = position.left + $this.outerWidth() - enlargeElement.outerWidth(true);
-            enlargeElement.css({ 
-                "top" : top, 
-                "left" : left,
-            });
-            enlargeElement.hover(
-                function() {
-                    var $this = $(this);
-                    $this.css("opacity", 1.0);
-                },
-                function() {
-                    $(this).remove();
-                }
-            ).click(event.target, function(event) {
-                var $this = $(this);
-                var $target = $(event.data);
-                var href = $target.closest('a').attr('href');
-                var initialTop = $target.top;
-                var initialLeft = $target.left;
-                var initialWidth = $target.width();
-                var initialHeight = $target.height();
-                $.get(href, function(data) {
-                    var $parsed = $('<div>').append(data);
-                    var medienSrc = $parsed.find('.works_display').children('a').children('img').attr('src');
-                    var bigSrc = medienSrc.replace('_m', '');
-                    $target.attr('src', medienSrc);
-                });
-                $target.css({
-                    'position' : 'fixed',
-                    'z-index' : 10000,
-                    //'width' : window.innerWidth,
-                    //'height' : window.innerHeight,
-                    'top' : Math.floor((window.innerHeight - initialHeight)/2),
-                    'left' : Math.floor(( window.innerWidth - initialWidth)/2)
-                });
-                $('body').append(
-                    $('<div>')
-                        .attr('id', 'shadow')
-                        .css({
-                            'position' : 'fixed',
-                            'background-color' : 'Black',
-                            'opacity' : 0.3,
-                            'top' : 0,
-                            'left' : 0,
-                            'height' : window.innerHeight,
-                            'width' : window.innerWidth,
-                            'z-index' : 9999
-                        })
-                        .click({'target' : $target, 'initialTop' : initialTop, 'initialLeft' : initialLeft}, function(event) {
-                            var $target = event.data.target;
-                            var initialTop = event.data.initialTop;
-                            var initialLeft = event.data.initialLeft;
-                            $target.css({
-                                'position' : 'static',
-                                'width' : initialWidth,
-                                'height' : initialHeight,
-                                'top' : initialTop,
-                                'left' : initialLeft
-                            });
-                            $(this).remove();
-                        })
-                );
-                $this.remove();
+                display : "none"
+                })
+        );
+    }; //end setEnlarge
 
-                event.preventDefault();
-            });
-        },
-        mouseleave : function() {
-            var $this = $(this);
-            //if mouse on #enlarge, don't remove #enlarge
-            if(!$("#enlarge").is(":hover")) {
-                $("#enlarge").remove();
-            }
-        }
-    });
+    //end function sets
+    
+    var IconUrl = setIconUrl();
+    setBox(IconUrl);
+    setEnlarge(IconUrl);
+    setThumbnail();
 });
